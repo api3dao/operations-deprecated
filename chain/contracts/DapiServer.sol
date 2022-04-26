b6554a074b5d4dcdc75547f02a0144046536d9be
bbenligiray <bbenligiray@gmail.com>
2022-04-26 20:27:14 +0300

    Add hardhat project

---
+++ chain/contracts/DapiServer.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@api3/airnode-protocol-v1/contracts/dapis/DapiServer.sol";
---
+++ chain/hardhat.config.js
@@ -0,0 +1,23 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('hardhat-deploy');
+
+const fs = require('fs');
+let credentials = require('./credentials.example.json');
+if (fs.existsSync('./credentials.json')) {
+  credentials = require('./credentials.json');
+}
+
+module.exports = {
+  etherscan: credentials.etherscan,
+  networks: credentials.networks,
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
