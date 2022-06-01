2e170dc985e8d467fa766b60d5076f81f6ac604c
Aaron Scheiner <blue@aquarat.co.za>
2022-06-01 14:09:27 +0200

    Add displayDisabled field to beacon-chains

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -26,6 +26,7 @@ export const extendedChainDescriptionSchema = z
     sponsor: z.string(),
     topUpWallets: z.array(topUpWalletSchema),
     updateConditionPercentage: z.number().optional(),
+    displayDisabled: z.boolean().optional(),
     airseekerConfig: z
       .object({
         deviationThreshold: z.number(),
