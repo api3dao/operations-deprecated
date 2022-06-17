808b357db7cb32e6b1f6bc9564fab78f701320c8
Aaron Scheiner <blue@aquarat.co.za>
2022-06-17 16:17:08 +0200

    Add logos to beacon metadata

--- src/deploy-airkeeper.ts
+++ src/deploy-airkeeper.ts
@@ -46,7 +46,7 @@ const main = async () => {
   );
   const awsSecretsFilePath = join(deploymentDirectory, 'aws.env');
 
-  const config = operationsRepository.apis[response.name].deployments[response.deployment].airkeeper.aws.config;
+  const config = operationsRepository.apis[response.name].deployments[response.deployment].airkeeper?.aws.config;
   const stage = config.nodeSettings.stage;
   const cloudProvider = config.nodeSettings.cloudProvider.type;
   if (cloudProvider === 'local') return cliPrint.error('🛑 Cloud provider is local. Please deploy to AWS/GCP.');
