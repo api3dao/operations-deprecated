2d86e53a3bbeda88259c8caf84f310e48ccda503
aquarat <blue@aquarat.co.za>
2022-09-02 12:29:28 +0200

    Refer to @api3/airnode-protocol for protocol IDs

--- src/utils/evm.ts
+++ src/utils/evm.ts
@@ -1,8 +1,6 @@
 import * as protocol from '@api3/airnode-protocol-v1';
 import { ethers } from 'ethers';
 
-export const PROTOCOL_ID_PSP = '2';
-
 export const getDapiServerInterface = () => {
   return new ethers.utils.Interface(protocol.DapiServer__factory.abi);
 };
