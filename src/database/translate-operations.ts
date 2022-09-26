3d109d72a0bb00e6d5c2ff6416953867829f2954
aquarat <blue@aquarat.co.za>
2022-09-26 12:58:32 +0200

    Add more steps to operations db loader

--- src/database/translate-operations.ts
+++ src/database/translate-operations.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, CloudType, ApplicationType } from '@prisma/client';
 import { readOperationsRepository } from '../utils/read-operations';
 
 const prisma = new PrismaClient();
@@ -11,6 +11,27 @@ export const getId = async (key: string) => {
   console.log('Query for', key, result);
   return result;
 };
+
+/*
+ * TODO vs Done
+ *  apis
+ *   - [x] beacons
+ *   - [x] deployments
+ *   - [x] ois
+ *   - [x] templates
+ *  beaconsets
+ *  [x] chains
+ *  dapis
+ *  explorer
+ *    - beaconMetadata
+ *    - beaconsetmetadata
+ *    - commonlogos
+ *    - dapiMetadata
+ *    - pricingCoverage
+ *   policies
+ *
+ *
+ */
 
 const main = async () => {
   // eslint-disable-next-line functional/immutable-data
@@ -55,14 +76,18 @@ const main = async () => {
       });
     })
   );
+  //WIP
+  // await Promise.all(Object.values(operations.beaconSets).map(async (beaconset) => {
+  //   await prisma.beaconSet.create({});
+  // }));
 
-  console.log(`Importing API Provider Metadata...`);
+  // Prisma doesn't support createMany with nested createMany, so...
   await Promise.all(
     Object.values(operations.apis).map(async (provider) => {
       const { name, airnode, description, homepage, logoPath, orderLogoPath, xpub, maxSubscriptionPeriod, active } =
         provider.apiMetadata;
 
-      await prisma.provider.create({
+      const createdProvider = await prisma.provider.create({
         data: {
           name,
           airnodeAddress: airnode,
@@ -74,29 +99,24 @@ const main = async () => {
           maxSubscriptionPeriod,
           active,
         },
+        select: { name: true },
       });
-    })
-  );
 
-  console.log(`Importing Templates...`);
-  await Promise.all(
-    Object.values(operations.apis).map(async (provider) => {
-      const templates = Object.values(provider.templates);
+      await Promise.all(
+        Object.values(operations.apis).map(async (provider) => {
+          const templates = Object.values(provider.templates);
 
-      await prisma.template.createMany({
-        data: templates.map(({ name, templateId, endpointId, parameters }) => ({
-          name,
-          templateId,
-          endpointId,
-          parameters,
-        })),
-      });
-    })
-  );
+          await prisma.template.createMany({
+            data: templates.map(({ name, templateId, endpointId, parameters }) => ({
+              name,
+              templateId,
+              endpointId,
+              parameters,
+            })),
+          });
+        })
+      );
 
-  // Prisma doesn't support createMany with nested createMany, so...
-  await Promise.all(
-    Object.values(operations.apis).map(async (provider) => {
       const beacons = Object.values(provider.beacons);
 
       await Promise.all(
@@ -156,6 +176,62 @@ const main = async () => {
               chains: { connect: createdChainConfig },
             },
           });
+
+          await prisma.oIS.createMany({
+            data: Object.entries(provider.ois).map(([name, ois]) => {
+              return {
+                name,
+                ois: JSON.stringify(ois),
+                version: ois.version,
+                providerName: createdProvider.name,
+              };
+            }),
+          });
+
+          (
+            await Promise.all(
+              Object.entries(provider.deployments).flatMap(async ([key, value]) => {
+                return await Promise.all(
+                  Object.entries(value.airnode).flatMap(async ([appType, next]) => {
+                    return await Promise.all(
+                      Object.entries(next).flatMap(async ([cloudType, airnodeNested]) => {
+                        const deployedAt = new Date(
+                          parseInt(`20${key.substring(0, 2)}`),
+                          parseInt(key.substring(2, 4)) - 1,
+                          parseInt(key.substring(4, 6)),
+                          parseInt(key.substring(7, 9)),
+                          parseInt(key.substring(9, 11))
+                        );
+
+                        return {
+                          applicationType: appType === 'airnode' ? ApplicationType.Airnode : ApplicationType.Airseeker,
+                          airnodeVersion: airnodeNested.airnodeVersion,
+                          deployedAt,
+                          cloudType: cloudType.toLowerCase() === 'GCP' ? CloudType.GCP : CloudType.AWS,
+                          providerName: createdProvider.name,
+                          config: JSON.stringify(airnodeNested.config),
+                          oisId: (
+                            await prisma.oIS.create({
+                              data: airnodeNested.config.ois,
+                              select: { id: true },
+                            })
+                          ).id,
+                        };
+                      })
+                    );
+                  })
+                );
+              })
+            )
+          )
+            .flat(10)
+            .map(async (airnode) => {
+              const insertable = { ...airnode, chainApiReference: undefined };
+
+              await prisma.deployment.create({
+                data: insertable,
+              });
+            });
         })
       );
     })
