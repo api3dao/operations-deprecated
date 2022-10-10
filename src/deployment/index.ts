e3fcaa1232c7296b7f9c792f3185db67a22f9750
Ashar2shahid <Ashar2shahid@gmail.com>
2022-10-10 17:09:24 -0400

    remove airseeker generation script

--- src/deployment/index.ts
+++
@@ -1,2 +0,0 @@
-export * as createBoilerPlate from './create-boilerplate';
-export * as createAirseekerConfig from './create-airseeker-config';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * as cli from './cli';
 export * as writeOperations from './write-operations';
 export * as readOperations from './read-operations';
 export * as filesystem from './filesystem';
+export * as createBoilerPlate from './create-boilerplate';
